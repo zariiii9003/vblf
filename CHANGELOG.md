96bf63ec4fd65d39aa44e2ae06026ad16d1f9188
zariiii9003 <zariiii9003@users.noreply.github.com>
2025-01-02 18:22:51 +0100

    packaging

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [v0.1.0] - YYYY-MM-DD
+- Initial release
+
+[v0.1.0]: https://example.com/compare/v0.0.0...v0.1.0
---
+++ docs/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "blf"
+copyright = "2024, Artur Drogunow"
+author = "Artur Drogunow"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# location of typehints
+autodoc_typehints = "description"
+
+# The default autodoc directive flags
+autodoc_default_options = {
+    "members": True,
+    "show-inheritance": True,
+}
+
+# decide whether module names are prepended to all object names
+add_module_names = False
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
